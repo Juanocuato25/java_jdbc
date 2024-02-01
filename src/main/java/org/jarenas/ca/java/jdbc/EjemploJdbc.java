23c8782f63ce39746d1de75ce21c805b3b0ad664
Juanocuato25 <juancarena2@hotmail.com>
2024-02-01 00:19:32 -0500

    Add packages model and repositorio with his files

--- src/main/java/org/jarenas/ca/java/jdbc/EjemploJdbc.java
+++ src/main/java/org/jarenas/ca/java/jdbc/EjemploJdbc.java
@@ -1,25 +1,24 @@
 package org.jarenas.ca.java.jdbc;
 
+import org.jarenas.ca.java.jdbc.util.ConexionBaseDatos;
+
 import java.sql.*;
 
 public class EjemploJdbc {
     public static void main(String[] args) {
 
-        String url = "jdbc:mysql://localhost:3306/java_curso";
-        String username = "root";
-        String password = "root";
-
-
         //Instanciando los objetos en los parantesis del try, usamos el autoclose de estos
-        try (Connection conn = DriverManager.getConnection(url, username, password);
+        try (Connection conn = ConexionBaseDatos.getInstance();
              Statement stmt = conn.createStatement();
              ResultSet resultado = stmt.executeQuery("SELECT * FROM productos")) {
 
             while (resultado.next()) {
+                System.out.print(resultado.getInt(1));
+                System.out.print(" -> ");
                 System.out.print(resultado.getString("nombre"));
-                System.out.print(" --> ");
-                System.out.print(resultado.getInt("precio"));
-                System.out.println("");
+                System.out.print(" -> ");
+                System.out.println(resultado.getInt("precio"));
+
             }
 
         } catch (SQLException e) {
---
+++ src/main/java/org/jarenas/ca/java/jdbc/repositorio/Repositorio.java
@@ -0,0 +1,12 @@
+package org.jarenas.ca.java.jdbc.repositorio;
+
+import java.util.List;
+
+public interface Repositorio <T>{
+    List<T>  listar();
+    T porId(Long id);
+
+    void guardar(T t);
+
+    void eliminar(Long id);
+}
